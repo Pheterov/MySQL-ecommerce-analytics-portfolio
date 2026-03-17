b6573b8183c8ca138b56c0e4e092e9f5e3e1e355
Piotr <110790920+Pheterov@users.noreply.github.com>
2026-03-17 16:54:40 +0100

    Update 1_analytics_portfolio_basics.sql
    query #4 enhanced version
    cosmetic changes

--- 1_analytics_portfolio_basics.sql
+++ 1_analytics_portfolio_basics.sql
@@ -161,16 +161,18 @@ FROM orders o;
 ============================================================================================================================================*/
 
 SELECT
-    pg.category                                          AS product_category
-    ,o.shipping_mode                                      AS shipping_type
+    pg.category                                          					product_category
+    ,o.shipping_mode                                      					shipping_type
     ,CASE 
-        WHEN COALESCE(op.position_discount, 0) > 0 THEN 'Discounted' 
+        WHEN COALESCE(op.position_discount, 0) > 0 
+		THEN 'Discounted' 
         ELSE 'Full Price' 
-    END                                                  AS discounted_flag
-    ,ROUND(AVG(DATEDIFF(o.shipping_date, o.order_date)), 2) AS avg_shipping_days
-    ,MIN(DATEDIFF(o.shipping_date, o.order_date))           AS min_shipping_days
-    ,MAX(DATEDIFF(o.shipping_date, o.order_date))           AS max_shipping_days
-    ,COUNT(DISTINCT o.order_id)                             AS orders_count
+    END                                                  					discounted_flag
+    ,ROUND(AVG(
+		DATEDIFF(o.shipping_date, o.order_date)), 2) 						avg_shipping_days
+    ,MIN(DATEDIFF(o.shipping_date, o.order_date))           				min_shipping_days
+    ,MAX(DATEDIFF(o.shipping_date, o.order_date))           				max_shipping_days
+    ,COUNT(DISTINCT o.order_id)                             				orders_count
 FROM orders o
 JOIN order_positions op ON o.order_id = op.order_id
 JOIN products p ON op.product_id = p.product_id
@@ -179,7 +181,10 @@ WHERE o.shipping_date >= o.order_date
 GROUP BY 
     pg.category, 
     o.shipping_mode,
-    CASE WHEN COALESCE(op.position_discount, 0) > 0 THEN 'Discounted' ELSE 'Full Price' END
+    CASE WHEN COALESCE(op.position_discount, 0) > 0
+		THEN 'Discounted'
+		ELSE 'Full Price'
+		END
 ORDER BY product_category, shipping_type, discounted_flag DESC;
 
 /*===================================================================================================
